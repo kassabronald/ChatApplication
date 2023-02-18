3927a9de67efde7865951746c9a4e5aaeadf8fb9
Jad Moukaddam <jim06@mail.aub.edu>
2023-02-18 18:01:00 +0200

    Uploadimagerequest created and imagescontroller updated

--- ChatApplication/ChatApplication.Web/Controllers/ImagesController.cs
+++ ChatApplication/ChatApplication.Web/Controllers/ImagesController.cs
@@ -2,58 +2,26 @@
 
 namespace ChatApplication.Controllers;
 
-public class ImagesController
+[ApiController]
+[Route("[controller]")]
+public class ImagesController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ImagesController : ControllerBase
+    private readonly IImageStore _imageStore;
+    
+    public ImagesController(IImageStore imageStore)
     {
-        private readonly IImageStore _imageStore;
-
-        public ImagesController(IImageStore imageStore)
+        _imageStore = imageStore;
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<FileContentResult>> 
+        DownloadImage(string id)
+    {
+        byte[] image = await _imageStore.GetImage(id);
+        if (image.isnull)
         {
-            _imageStore = imageStore;
+            return NotFound($"An image with id {id} was not found");
         }
-
-        [HttpGet("{username}")]
-        public async Task<ActionResult<Image>> GetImage(string username)
-        {
-            var image = await _imageStore.GetImage(username);
-            if (image == null)
-            {
-                return NotFound($"An image for user {username} was not found");
-            }
-
-            return Ok(image);
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<Image>> AddImage(Image image)
-        {
-            var existingImage = await _imageStore.GetImage(image.username);
-            if (existingImage != null)
-            {
-                return Conflict($"An image for user {image.username} already exists");
-            }
-
-            await _imageStore.AddImage(image);
-            return CreatedAtAction(nameof(GetImage), new {username = image.username},
-                image);
-        }
-
-        [HttpPut("{username?}")]
-        public async Task<ActionResult<Image>> UpdateImage(string username, PutImageRequest request)
-        {
-            var existingImage = await _imageStore.GetImage(username);
-            if (existingImage == null)
-            {
-                return NotFound($"An image for user {username} was not found");
-            }
-
-            var image = new Image(username, request.image);
-            await _imageStore.UpsertImage(image);
-            return Ok(image);
-        }
+        return File(image, "image/png");
     }
     
     
---
+++ ChatApplication/ChatApplication.Web/Dtos/UploadImageRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace ChatApplication.Web.Dtos;
+
+
+public record UploadImageRequest(IFormFile File);
