ce946ba7e11e78dfcfdde72d00c9a30335353ce6
Ronald Kassab <66827298+kassabronald@users.noreply.github.com>
2023-02-18 21:36:29 +0200

    Worked on Image Controller Tests

--- ChatApplication/ChatApplication.Web/Controllers/ImagesController.cs
+++ ChatApplication/ChatApplication.Web/Controllers/ImagesController.cs
@@ -15,14 +15,16 @@ public class ImagesController : ControllerBase
         _imageStore = imageStore;
     }
     [HttpGet("{id}")]
-    public async Task<ActionResult<FileContentResult>> DownloadImage(string id)
+    public async Task<ActionResult<FileContent>> DownloadImage(string id)
     {
         var image = await _imageStore.GetImage(id);
         if (image == null)
         {
             return NotFound($"An image with id {id} was not found");
         }
-        return File(image, "image/png");
+        FileContent fileContentResult = new(image, "image/jpeg");        
+        return Ok(fileContentResult);
+
     }
     
     [HttpPost]
