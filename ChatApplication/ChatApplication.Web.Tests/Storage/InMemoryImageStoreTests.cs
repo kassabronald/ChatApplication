675eee4c88fb0d370c9b738bf6259eed1d81669e
Jad Moukaddam <jim06@mail.aub.edu>
2023-02-19 19:13:28 +0200

    Made inmemoryprofilestore and imagestore with their tests

---
+++ ChatApplication/ChatApplication.Web.Tests/Storage/InMemoryImageStoreTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using ChatApplication.Storage;
+
+namespace ChatApplication.Web.Tests.Storage;
+
+public class InMemoryImageStoreTests
+{
+    private readonly InMemoryImageStore _store = new();
+    
+    [Fact]
+    public async Task AddImage()
+    {
+        var data = new MemoryStream();
+        await data.WriteAsync(Encoding.UTF8.GetBytes("foobar"));
+        var blobName = "foobar";
+        var id=await _store.AddImage(blobName, data);
+        Assert.Equal(data.ToArray(), await _store.GetImage(id));
+    }
+    
+    [Fact]
+    public async Task GetNonExistingImage()
+    {
+        Assert.Null(await _store.GetImage("foobar"));
+    }
+    
+    [Theory]
+    [InlineData(null, new byte[0])]
+    [InlineData("", new byte[0])]
+    [InlineData(" ", new byte[0])]
+    [InlineData("foobar", new byte[0])]
+    public async Task AddImage_InvalidArgs(string blobname, byte[] data)
+    {
+        await Assert.ThrowsAsync<ArgumentException>(async () =>
+        {
+            await _store.AddImage(blobname, new MemoryStream(data));
+        });
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public async Task GetImage_InvalidArgs(string id)
+    {
+        Assert.Null( await _store.GetImage(id));
+        
+    }
+    
+    
+}
---
+++ ChatApplication/ChatApplication.Web/Storage/InMemoryImageStore.cs
@@ -0,0 +1,36 @@
+﻿namespace ChatApplication.Storage;
+
+public class InMemoryImageStore : IImageStore
+{
+    private readonly Dictionary<string, byte[]> _images = new();
+
+    public async Task<string?> AddImage(string blobName, MemoryStream data)
+    {
+        if (data==null || data.Length==0 || string.IsNullOrWhiteSpace(blobName))
+        {
+            throw new ArgumentException($"Missing arguments");
+        }
+
+        var id="";
+        while (_images.ContainsKey(id = Guid.NewGuid().ToString()))
+        {
+        }
+
+        _images.Add(id, data.ToArray());
+        return id;
+    }
+
+    public async Task<byte[]?> GetImage(string id)
+    {
+        if (string.IsNullOrWhiteSpace(id))
+        {
+            return null;
+        }
+        if (_images.TryGetValue(id, out var image))
+        {
+            return image;
+        }
+
+        return null;
+    }
+}
