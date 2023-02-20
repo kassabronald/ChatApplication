07e946ae4d9d735855f173ff0e5207ccaf184558
Ronald Kassab <66827298+kassabronald@users.noreply.github.com>
2023-02-21 00:58:58 +0200

    Removed an unecessary file

--- ChatApplication/ChatApplication.Web/Controllers/FileContent.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace ChatApplication.Web.Dtos;
-
-public record FileContent(
-    [Required] byte [] Data,
-    [Required] string ContentType
-    );
