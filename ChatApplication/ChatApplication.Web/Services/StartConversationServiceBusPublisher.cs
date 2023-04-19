82a329e793d61bd0204a62db3fbf4eba7ae64188
Jad Moukaddam <jim06@mail.aub.edu>
2023-04-19 23:10:33 +0300

    added bus publisher for start conversation

--- ChatApplication/ChatApplication.Web/Services/StartConversationServiceBusPublisher.cs
+++ ChatApplication/ChatApplication.Web/Services/StartConversationServiceBusPublisher.cs
@@ -1,6 +1,51 @@
+using Azure.Messaging.ServiceBus;
+using ChatApplication.Configuration;
+using ChatApplication.Serializers.Implementations;
+using Microsoft.Extensions.Options;
+
 namespace ChatApplication.Services;
 
-public class StartConversationServiceBusPublisher
+public class StartConversationServiceBusPublisher : IHostedService
 {
+    private readonly IConversationService _conversationService;
+    private readonly IStartConversationParametersSerializer _startConversationSerializer;
+    private readonly ServiceBusProcessor _startConversationServiceBusProcessor;
     
+    public StartConversationServiceBusPublisher(IConversationService conversationService, IStartConversationParametersSerializer startConversationSerializer, ServiceBusClient serviceBusClient, IOptions<ServiceBusSettings> options)
+    {
+        _conversationService = conversationService;
+        _startConversationSerializer = startConversationSerializer;
+        _startConversationServiceBusProcessor = serviceBusClient.CreateProcessor(options.Value.StartConversationQueueName);
+        
+        // add handler to process messages
+        _startConversationServiceBusProcessor.ProcessMessageAsync += MessageHandler;
+
+        // add handler to process any errors
+        _startConversationServiceBusProcessor.ProcessErrorAsync += ErrorHandler;
+    }
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return _startConversationServiceBusProcessor.StartProcessingAsync(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return _startConversationServiceBusProcessor.StopProcessingAsync(cancellationToken);
+    }
+    
+    private async Task MessageHandler(ProcessMessageEventArgs args)
+    {
+        string data = args.Message.Body.ToString();
+
+        var startConversationParameters = _startConversationSerializer.DeserializeStartConversationParameters(data);
+        await _conversationService.StartConversation(startConversationParameters);
+
+        await args.CompleteMessageAsync(args.Message);
+    }
+    
+    private Task ErrorHandler(ProcessErrorEventArgs args)
+    {
+        Console.WriteLine(args.Exception.ToString());
+        return Task.CompletedTask;
+    }
 }
