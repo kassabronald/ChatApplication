726fde643e86ed0124e4218130132904aee5f5e6
Jad Moukaddam <jim06@mail.aub.edu>
2023-04-19 22:41:53 +0300

    added service bus settings

--- ChatApplication/ChatApplication.Web/ServiceBus/Implementations/StartConversationServiceBusPublisher.cs
+++ ChatApplication/ChatApplication.Web/ServiceBus/Implementations/StartConversationServiceBusPublisher.cs
@@ -1,6 +1,29 @@
+using Azure.Messaging.ServiceBus;
+using ChatApplication.Configuration;
+using ChatApplication.Serializers.Implementations;
+using ChatApplication.Web.Dtos;
+using Microsoft.Extensions.Options;
+
 namespace ChatApplication.ServiceBus;
 
 public class StartConversationServiceBusPublisher
 {
+    private readonly ServiceBusSender _sender;
+    private readonly IStartConversationParametersSerializer _serializer;
+    
+    public StartConversationServiceBusPublisher(ServiceBusSender sender, IStartConversationParametersSerializer serializer,
+        IOptions<ServiceBusSettings> options
+    )
+    {
+        _sender = sender;
+        _serializer = serializer;
+    }
+    
+    public async Task Send(StartConversationParameters parameters)
+    {
+        var serializedParameters = _serializer.SerializeStartConversationParametersSerializer(parameters);
+        await _sender.SendMessageAsync(new ServiceBusMessage(serializedParameters));
+    }
+    
     
 }
