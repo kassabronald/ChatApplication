8ee07dd7da410d61add24af42786d03156f101ef
Jad Moukaddam <jim06@mail.aub.edu>
2023-04-26 22:39:53 +0300

    safe commit

--- ChatApplication/ChatApplication.Web/Storage/StoragesImplementations/SQL/SQLMessageStore.cs
+++ ChatApplication/ChatApplication.Web/Storage/StoragesImplementations/SQL/SQLMessageStore.cs
@@ -1,6 +1,27 @@
+using ChatApplication.Web.Dtos;
+
 namespace ChatApplication.Storage.SQL;
 
-public class SQLMessageStore
+public class SQLMessageStore : IMessageStore
 {
     
+    public Task AddMessage(Message message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GetMessagesResult> GetMessages(GetMessagesParameters parameters)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteMessage(Message message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Message> GetMessage(string conversationId, string messageId)
+    {
+        throw new NotImplementedException();
+    }
 }
